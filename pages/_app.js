7e04d47a5eb7689f79c73ce5695a27d9552e7f61
AN <108299225+krakxn@users.noreply.github.com>
2022-07-24 12:16:15 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,9 @@
-import { MoralisProvider } from "react-moralis";
-import "../styles/globals.css";
-import Header from "../components/Header";
+import "../styles/globals.css"
+import { MoralisProvider } from "react-moralis"
+import Header from "../components/Header"
+import Head from "next/head"
+import { NotificationProvider } from "web3uikit"
+
 
 function MyApp({ Component, pageProps }) {
   return (
